82e9e4ddba22269ff1476d3393409e038adb9432
Joonas Lehtinen <644969+jojule@users.noreply.github.com>
2005-03-23 10:57:48 +0000

    Made action order a bit less random.

--- base/src/org/millstone/base/ui/Tree.java
+++ base/src/org/millstone/base/ui/Tree.java
@@ -42,6 +42,7 @@ import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
@@ -331,7 +332,7 @@ public class Tree extends Select implements Container.Hierarchical, Action.Conta
 			target.addAttribute("allownewitem", true);
 
 		// Initialize variables
-		HashSet actionSet = new HashSet();
+		Set actionSet = new LinkedHashSet();
 		String[] selectedKeys;
 		if (isMultiSelect())
 			selectedKeys = new String[((Set) getValue()).size()];
