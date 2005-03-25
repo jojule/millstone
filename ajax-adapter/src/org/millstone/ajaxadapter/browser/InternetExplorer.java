5f4c34739ec3e1edf4d910fa040b7b3b436c7e50
Joonas Lehtinen <644969+jojule@users.noreply.github.com>
2005-03-25 10:26:23 +0000

    Stubs

---
+++ ajax-adapter/src/org/millstone/ajaxadapter/browser/InternetExplorer.java
@@ -0,0 +1,50 @@
+/* **********************************************************************
+
+Millstone 
+Open Sourced User Interface Library for
+Internet Development with Java
+
+Millstone is a registered trademark of IT Mill Ltd
+Copyright 2000-2005 IT Mill Ltd
+
+*************************************************************************
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*************************************************************************
+
+For more information, contact:
+
+IT Mill Ltd                           phone: +358 2 4802 7180
+Ruukinkatu 2-4                        fax:  +358 2 4802 7181
+20540, Turku                          email: info@itmill.com
+Finland                               company www: www.itmill.com
+
+Primary source for MillStone information and releases: www.millstone.org
+
+********************************************************************** */
+
+
+package org.millstone.ajaxadapter.browser;
+
+/** Internet Explorer specific terminal implementation of terminal interface.
+ * 
+ * (stage drafting and planning)
+ * 
+ * @author IT Mill Ltd, Joonas Lehtinen
+ */
+public class InternetExplorer extends AbstractBrowser {
+
+}
