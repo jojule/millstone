2ded3589315eabe28c777f142bfea75d7b582a95
Sami Ekblad <sami@vaadin.com>
2005-10-14 13:09:11 +0000

    Support errorListener

--- ajax-adapter/src/org/millstone/ajaxadapter/VariableMap.java
+++ ajax-adapter/src/org/millstone/ajaxadapter/VariableMap.java
@@ -1,40 +1,40 @@
-/* **********************************************************************
+/* *************************************************************************
+ 
+   								Millstone(TM) 
+   				   Open Sourced User Interface Library for
+   		 		       Internet Development with Java
 
- Millstone 
- Open Sourced User Interface Library for
- Internet Development with Java
+             Millstone is a registered trademark of IT Mill Ltd
+                  Copyright (C) 2000,2001,2002 IT Mill Ltd
+                     
+   *************************************************************************
 
- Millstone is a registered trademark of IT Mill Ltd
- Copyright 2000-2005 IT Mill Ltd
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
- *************************************************************************
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
 
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
+   *************************************************************************
+   
+   For more information, contact:
+   
+   IT Mill Ltd                           phone: +358 2 4802 7180
+   Ruukinkatu 2-4                        fax:  +358 2 4802 7181
+   20540, Turku                          email: info@itmill.com
+   Finland                               company www: www.itmill.com
+   
+   Primary source for MillStone information and releases: www.millstone.org
 
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
- *************************************************************************
-
- For more information, contact:
-
- IT Mill Ltd                           phone: +358 2 4802 7180
- Ruukinkatu 2-4                        fax:  +358 2 4802 7181
- 20540, Turku                          email: info@itmill.com
- Finland                               company www: www.itmill.com
-
- Primary source for MillStone information and releases: www.millstone.org
-
- ********************************************************************** */
+   ********************************************************************** */
 
 package org.millstone.ajaxadapter;
 
@@ -455,7 +455,8 @@ public class VariableMap {
 	*  @return Name to Value[] mapping of unhandled variables
 	*/
 	public Map handleVariables(
-		HttpServletRequest req)
+		HttpServletRequest req,
+		Terminal.ErrorListener errorListener)
 		throws IOException {
 
 		// Get the parameters
@@ -563,6 +564,8 @@ public class VariableMap {
 										Log.except(
 											"WebVariableMap conversion exception",
 											e);
+										errorListener.terminalError(
+											new TerminalErrorImpl(e));
 									}
 								}
 							}
@@ -576,6 +579,8 @@ public class VariableMap {
 						listener.changeVariables(req, variables);
 					} catch (Throwable t) {
 						// Notify the error listener
+						errorListener.terminalError(
+							new VariableOwnerErrorImpl(listener, t));
 					}
 				}
 			}
@@ -731,5 +736,4 @@ public class VariableMap {
 		resultNormal.addAll(resultImmediate);
 		return resultNormal;
 	}
-
 }
